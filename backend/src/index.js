e780ddc094d2988d38ff466cd43182c823b5d917
matheus.s.pinheiro@live.com <matheus.s.pinheiro@live.com>
2023-03-30 13:46:51 -0300

    CORSFix

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,16 +5,9 @@ const cors = require('cors')
 
 const app = express()
 
-const corsOptions = {
-  origin: 'http://localhost:3000',
-  credentials: true,
-  optionSuccessStatus: 200
-}
-
-app.use(cors(corsOptions))
-
 app.use(express.json())
+app.use(cors())
 app.use(rotas)
 
 const port = process.env.PORT || 3000
-app.listen(3000)
+app.listen(port)
